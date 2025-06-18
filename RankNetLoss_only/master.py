5d227da6b8cb4ac2869706970a825891c551d7a4
DevOpProgrammer <q56134071@gs.ncku.edu.tw>
2025-06-18 15:49:25 +0800

    Add files via upload

---
+++ RankNetLoss_only/master.py
@@ -0,0 +1,276 @@
+import torch
+from torch import nn
+from torch.nn.modules.linear import Linear
+from torch.nn.modules.dropout import Dropout
+from torch.nn.modules.normalization import LayerNorm
+import math
+
+from base_model import SequenceModel
+
+#Droppath
+class DropPath(nn.Module):
+    def __init__(self, drop_prob=0.0):
+        super(DropPath, self).__init__()
+        self.drop_prob = drop_prob
+
+    def forward(self, x):
+        if self.drop_prob == 0.0 or not self.training:
+            return x
+        keep_prob = 1 - self.drop_prob
+        shape = (x.shape[0],) + (1,) * (x.ndim - 1)  # [B, 1, 1, ...]
+        random_tensor = keep_prob + torch.rand(shape, dtype=x.dtype, device=x.device)
+        random_tensor = torch.floor(random_tensor)
+        return x / keep_prob * random_tensor
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_len=100):
+        super(PositionalEncoding, self).__init__()
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        self.register_buffer("pe", pe)
+
+    def forward(self, x):
+        return x + self.pe[:x.shape[1], :]
+
+
+class SAttention(nn.Module):
+    def __init__(self, d_model, nhead, dropout, drop_path=0.0):
+        super().__init__()
+
+        self.d_model = d_model
+        self.nhead = nhead
+        self.temperature = math.sqrt(self.d_model/nhead)
+
+        self.qtrans = nn.Linear(d_model, d_model, bias=False)
+        self.ktrans = nn.Linear(d_model, d_model, bias=False)
+        self.vtrans = nn.Linear(d_model, d_model, bias=False)
+
+        attn_dropout_layer = []
+        for i in range(nhead):
+            attn_dropout_layer.append(Dropout(p=dropout))
+        self.attn_dropout = nn.ModuleList(attn_dropout_layer)
+
+        # input LayerNorm
+        self.norm1 = LayerNorm(d_model, eps=1e-5)
+        # FFN layerNorm
+        self.norm2 = LayerNorm(d_model, eps=1e-5)
+
+        # Linear → ReLU → Linear
+        """
+        self.ffn = nn.Sequential(
+            Linear(d_model, d_model),
+            nn.ReLU(),
+            Dropout(p=dropout),
+            Linear(d_model, d_model),
+            Dropout(p=dropout)
+        )
+        """
+        #GLU
+        self.ffn = nn.Sequential(
+            nn.Linear(d_model, d_model * 2),
+            nn.GLU(),  # Gated Linear Unit
+            nn.Dropout(p=dropout),
+            nn.Linear(d_model, d_model),
+            nn.Dropout(p=dropout)
+        )
+        self.drop_path = DropPath(drop_prob=drop_path) if drop_path > 0 else nn.Identity()
+
+    def forward(self, x):
+        x = self.norm1(x)
+        q = self.qtrans(x).transpose(0,1)
+        k = self.ktrans(x).transpose(0,1)
+        v = self.vtrans(x).transpose(0,1)
+
+        dim = int(self.d_model/self.nhead)
+        att_output = []
+        for i in range(self.nhead):
+            if i==self.nhead-1:
+                qh = q[:, :, i * dim:]
+                kh = k[:, :, i * dim:]
+                vh = v[:, :, i * dim:]
+            else:
+                qh = q[:, :, i * dim:(i + 1) * dim]
+                kh = k[:, :, i * dim:(i + 1) * dim]
+                vh = v[:, :, i * dim:(i + 1) * dim]
+
+            atten_ave_matrixh = torch.softmax(torch.matmul(qh, kh.transpose(1, 2)) / self.temperature, dim=-1)
+            if self.attn_dropout:
+                atten_ave_matrixh = self.attn_dropout[i](atten_ave_matrixh)
+            att_output.append(torch.matmul(atten_ave_matrixh, vh).transpose(0, 1))
+        att_output = torch.concat(att_output, dim=-1)
+
+        # FFN
+        xt = x + att_output
+        xt = self.norm2(xt)
+        att_output = xt + self.ffn(xt)
+        #droppath
+        att_output = self.drop_path(att_output)
+
+        return att_output
+
+
+class TAttention(nn.Module):
+    def __init__(self, d_model, nhead, dropout, drop_path=0.0):
+        super().__init__()
+        self.d_model = d_model
+        self.nhead = nhead
+        self.qtrans = nn.Linear(d_model, d_model, bias=False)
+        self.ktrans = nn.Linear(d_model, d_model, bias=False)
+        self.vtrans = nn.Linear(d_model, d_model, bias=False)
+        #Droppath
+        self.drop_path = DropPath(drop_prob=drop_path) if drop_path > 0 else nn.Identity()
+
+
+        self.attn_dropout = []
+        if dropout > 0:
+            for i in range(nhead):
+                self.attn_dropout.append(Dropout(p=dropout))
+            self.attn_dropout = nn.ModuleList(self.attn_dropout)
+
+        # input LayerNorm
+        self.norm1 = LayerNorm(d_model, eps=1e-5)
+        # FFN layerNorm
+        self.norm2 = LayerNorm(d_model, eps=1e-5)
+        # Linear → ReLU → Linear
+        """
+        self.ffn = nn.Sequential(
+            Linear(d_model, d_model),
+            nn.ReLU(),
+            Dropout(p=dropout),
+            Linear(d_model, d_model),
+            Dropout(p=dropout)
+        )
+        """
+        
+        #GLU
+        self.ffn = nn.Sequential(
+            nn.Linear(d_model, d_model * 2),
+            nn.GLU(),  # Gated Linear Unit
+            nn.Dropout(p=dropout),
+            nn.Linear(d_model, d_model),
+            nn.Dropout(p=dropout)
+        )
+        self.drop_path = DropPath(drop_prob=drop_path) if drop_path > 0 else nn.Identity()
+        
+        
+    def forward(self, x):
+        x = self.norm1(x)
+        q = self.qtrans(x)
+        k = self.ktrans(x)
+        v = self.vtrans(x)
+
+        dim = int(self.d_model / self.nhead)
+        att_output = []
+        for i in range(self.nhead):
+            if i==self.nhead-1:
+                qh = q[:, :, i * dim:]
+                kh = k[:, :, i * dim:]
+                vh = v[:, :, i * dim:]
+            else:
+                qh = q[:, :, i * dim:(i + 1) * dim]
+                kh = k[:, :, i * dim:(i + 1) * dim]
+                vh = v[:, :, i * dim:(i + 1) * dim]
+            atten_ave_matrixh = torch.softmax(torch.matmul(qh, kh.transpose(1, 2)), dim=-1)
+            if self.attn_dropout:
+                atten_ave_matrixh = self.attn_dropout[i](atten_ave_matrixh)
+            att_output.append(torch.matmul(atten_ave_matrixh, vh))
+        att_output = torch.concat(att_output, dim=-1)
+
+        # FFN
+        xt = x + att_output
+        xt = self.norm2(xt)
+        att_output = xt + self.ffn(xt)
+        #droppath
+        att_output = self.drop_path(att_output)
+        return att_output
+
+
+class Gate(nn.Module):
+    def __init__(self, d_input, d_output,  beta=1.0):
+        super().__init__()
+        self.trans = nn.Linear(d_input, d_output)
+        self.d_output =d_output
+        self.t = beta
+
+    def forward(self, gate_input):
+        output = self.trans(gate_input)
+        output = torch.softmax(output/self.t, dim=-1)
+        return self.d_output*output
+
+
+class TemporalAttention(nn.Module):
+    def __init__(self, d_model):
+        super().__init__()
+        self.trans = nn.Linear(d_model, d_model, bias=False)
+
+    def forward(self, z):
+        h = self.trans(z) # [N, T, D]
+        query = h[:, -1, :].unsqueeze(-1)
+        lam = torch.matmul(h, query).squeeze(-1)  # [N, T, D] --> [N, T]
+        lam = torch.softmax(lam, dim=1).unsqueeze(1)
+        output = torch.matmul(lam, z).squeeze(1)  # [N, 1, T], [N, T, D] --> [N, 1, D]
+        return output
+
+
+class MASTER(nn.Module):
+    def __init__(self, d_feat, d_model, t_nhead, s_nhead, T_dropout_rate, S_dropout_rate, gate_input_start_index, gate_input_end_index, beta, drop_path=0.0):
+        super(MASTER, self).__init__()
+        # market
+        self.gate_input_start_index = gate_input_start_index
+        self.gate_input_end_index = gate_input_end_index
+        self.d_gate_input = (gate_input_end_index - gate_input_start_index) # F'
+        self.feature_gate = Gate(self.d_gate_input, d_feat, beta=beta)
+
+        self.layers = nn.Sequential(
+            # feature layer
+            nn.Linear(d_feat, d_model),
+            PositionalEncoding(d_model),
+            # intra-stock aggregation
+            TAttention(d_model=d_model, nhead=t_nhead, dropout=T_dropout_rate, drop_path=drop_path),
+            # inter-stock aggregation
+            SAttention(d_model=d_model, nhead=s_nhead, dropout=S_dropout_rate, drop_path=drop_path),
+            TemporalAttention(d_model=d_model),
+            # decoder
+            nn.Linear(d_model, 1)
+        )
+
+    def forward(self, x):
+        src = x[:, :, :self.gate_input_start_index] # N, T, D
+        gate_input = x[:, -1, self.gate_input_start_index:self.gate_input_end_index]
+        src = src * torch.unsqueeze(self.feature_gate(gate_input), dim=1)
+       
+        output = self.layers(src).squeeze(-1)
+
+        return output
+
+
+class MASTERModel(SequenceModel):
+    def __init__(
+            self, d_feat, d_model, t_nhead, s_nhead, gate_input_start_index, gate_input_end_index,
+            T_dropout_rate, S_dropout_rate, beta, drop_path=0.0, **kwargs,
+    ):
+        super(MASTERModel, self).__init__(**kwargs)
+        self.d_model = d_model
+        self.d_feat = d_feat
+
+        self.gate_input_start_index = gate_input_start_index
+        self.gate_input_end_index = gate_input_end_index
+
+        self.T_dropout_rate = T_dropout_rate
+        self.S_dropout_rate = S_dropout_rate
+        self.t_nhead = t_nhead
+        self.s_nhead = s_nhead
+        self.beta = beta
+        self.drop_path = drop_path
+
+        self.init_model()
+
+    def init_model(self):
+        self.model = MASTER(d_feat=self.d_feat, d_model=self.d_model, t_nhead=self.t_nhead, s_nhead=self.s_nhead,
+                                   T_dropout_rate=self.T_dropout_rate, S_dropout_rate=self.S_dropout_rate,
+                                   gate_input_start_index=self.gate_input_start_index,
+                                   gate_input_end_index=self.gate_input_end_index, beta=self.beta, drop_path=self.drop_path)
+        super(MASTERModel, self).init_model()
